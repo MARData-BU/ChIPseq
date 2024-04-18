3fdcd465a816f50864c6302d600b9d1b592c68d3
pberenguermolins <146096716+pberenguermolins@users.noreply.github.com>
2024-04-18 16:50:27 +0200

    Update fastq_files_names.sh

--- functions/fastq_files_names.sh
+++ functions/fastq_files_names.sh
@@ -2,18 +2,22 @@
 #SBATCH -p lowmem           # Partition to submit to
 #SBATCH --cpus-per-task=2
 #SBATCH --mem-per-cpu 5Gb     # Memory in MB
-#SBATCH -J TrimCutadapt           # job name
-#SBATCH -o logs/TrimCutadapt.%j.out    # File to which standard out will be written
-#SBATCH -e logs/TrimCutadapt.%j.err    # File to which standard err will be written
+#SBATCH -J ChangeNames           # job name
+#SBATCH -o logs/ChangeNames.%j.out    # File to which standard out will be written
+#SBATCH -e logs/ChangeNames.%j.err    # File to which standard err will be written
 
 #-------------------------------
 
 module load R/4.2.1-foss-2020b
 
-
 FUNCTIONSDIR=$1
 SAMPLE_SHEET=$2
-OUTDIR=$3
-INPUTDIR=$4
+INPUTDIR=$3
+OUTDIR=$4
 
-Rscript $FUNCTIONSDIR/fastq_files_changing_names.R $INPUTDIR $sample_sheet $OUTDIR
+echo -e "Functions directory is $FUNCTIONSDIR."
+echo -e "Sample sheet is $SAMPLE_SHEET."
+echo -e "Input directory is $INPUTDIR."
+echo -e "Output directory is $OUTDIR."
+
+Rscript $FUNCTIONSDIR/fastq_files_changing_names.R $INPUTDIR $SAMPLE_SHEET $OUTDIR
